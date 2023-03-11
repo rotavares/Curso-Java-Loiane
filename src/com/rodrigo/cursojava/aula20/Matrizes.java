b9d9d154e559ca09e7fce1dedb21137ea0222c40
caroline <caroline.ferrero@outlook.com.br>
2023-03-11 14:06:21 -0300

    Aula Matrizes

--- src/com/rodrigo/cursojava/aula20/Matrizes.java
+++ src/com/rodrigo/cursojava/aula20/Matrizes.java
@@ -4,21 +4,37 @@ public class Matrizes {
 
 	public static void main(String[] args) {
 		
-		double[][] notasAlunos = new double[30][4];
+		double[][] notasAlunos = {{10, 7, 9, 9.5}, {9, 8, 7, 9}, {8, 9, 10, 7}};
 		
-		notasAlunos[0][0] = 10;
-		notasAlunos[0][1] = 7;
-		notasAlunos[0][2]= 9;
-		notasAlunos[0][3] = 9.5;
+		for (int i = 0; i < notasAlunos.length; i++) {
+			
+			System.out.print("Notas do " + (i+1) + " aluno, ");
+			
+			for (int j = 0; j < notasAlunos[i].length; j++) {
+				
+				System.out.print(notasAlunos[i][j]);
+				
+				if (j != 3) {
+					
+					System.out.print(" - ");
+				}
+			}
+			
+			System.out.println();
+		}
+		
+		double soma;
+		
+		for (int i = 0; i < notasAlunos.length; i++) {
+			
+			soma = 0;
+			
+			for (int j = 0; j < notasAlunos[i].length; j++) {
+				
+				soma += notasAlunos[i][j];
 
-		notasAlunos[1][0] = 9;
-		notasAlunos[1][1] = 8;
-		notasAlunos[1][2]= 7;
-		notasAlunos[1][3] = 9;
-
-		notasAlunos[2][0] = 8;
-		notasAlunos[2][1] =9;
-		notasAlunos[2][2]= 10;
-		notasAlunos[2][3] = 7;
+			}
+			System.out.println("Média do aluno " + (i+1) + " = " + (soma / 4));
+		}
 	}
 }
