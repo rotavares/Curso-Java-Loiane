d7ddc6926c42ecee36f2bb89bbd57ca7fcd472c8
rokalala <rodrigotavaresrt7@gmail.com>
2023-03-22 21:01:38 -0300

    Ex5 completo, Ex6 incompleto

---
+++ src/com/rodrigo/cursojava/aula20/labs/Ex6.java
@@ -0,0 +1,16 @@
+package com.rodrigo.cursojava.aula20.labs;
+
+import java.util.Scanner;
+
+public class Ex6 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		String[][] jogoVelha = new String[3][3];
+		
+		System.out.println("Jogador 1 = O\n"
+				+ "Jogador 2 = X");	
+	}
+}
