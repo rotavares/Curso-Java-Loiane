34b6f6af59e5958094faa1a70c1c23de66920ad6
rokalala <rodrigotavaresrt7@gmail.com>
2023-03-29 22:14:07 -0300

    ex02 incompleto

--- src/com/rodrigo/cursojava/aula27/labs/ContaCorrente.java
+++ src/com/rodrigo/cursojava/aula27/labs/ContaCorrente.java
@@ -1,5 +1,7 @@
 package com.rodrigo.cursojava.aula27.labs;
 
+import java.util.Scanner;
+
 public class ContaCorrente {
 
 	String numero;
@@ -13,15 +15,121 @@ public class ContaCorrente {
 		double saldoAtual;
 
 		if (especial) {
-			
+
 			limiteEspecial = limiteEspecial - valor;
 
 			return limiteEspecial;
 		} else {
-			
+
 			saldoAtual = saldo - valor;
+
+			saldo = saldoAtual;
 
 			return saldoAtual;
 		}
 	}
+
+	double depositarDinheiro(double valor) {
+
+		double saldoAtual = saldo + valor;
+
+		return saldoAtual;
+	}
+
+	double consultarSaldo(boolean consultarEspecial) {
+
+		if (consultarEspecial) {
+
+			return limiteEspecial;
+		} else {
+
+			return saldo;
+		}
+	}
+	
+	void IniciarMenu() {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		boolean sair = false;
+		
+		while (!sair) {
+			
+			System.out.println("'1' Consultar saldo | '2' Depositar | '3' Sacar | '0' Sair");
+			
+			System.out.println();
+			
+			byte escolha = scan.nextByte();
+			
+			switch (escolha) {
+			case 1:
+					System.out.println("Deseja consultar também o limite especial? : s/N ");
+					
+					char resposta = scan.next().charAt(0);
+					
+					if (especial) {
+						
+						if (resposta == 's' || resposta == 'S') {
+							
+							System.out.println("Limite especial = " + consultarSaldo(especial));
+							
+							System.out.println("Saldo = " + consultarSaldo(false));
+							
+						} else if (resposta == 'n' || resposta == 'N'){
+							
+							System.out.println("Saldo = " + consultarSaldo(false));
+							
+						}
+						
+					} else {
+
+						System.out.println("Saldo = " + consultarSaldo(especial));
+						
+					}
+					
+				break;
+				
+			case 2:
+				
+				System.out.println("Digite o valor do deposito: ");
+				
+				double valorDeposito = scan.nextDouble();
+				
+				depositarDinheiro(valorDeposito);
+				
+				System.out.println("Deposito efetuado com sucesso!");
+				
+				break;
+
+			case 3:
+				
+				System.out.println("Digite o valor para saque: ");
+				
+				double valorSaque = scan.nextDouble();
+				
+				sacarDinheiro(valorSaque);
+				
+				System.out.println("Seu saldo atual = " + consultarSaldo(false));
+				
+				break;
+
+			case 0:
+				
+				sair = true;
+				
+				break;
+				
+			default:
+				
+				System.out.println("Número inválido, digite novamente.");
+				
+				break;
+			}
+			
+		}
+		
+		scan.close();
+		
+	}
+	
 }
---
+++ src/com/rodrigo/cursojava/aula27/labs/Ex02.java
@@ -0,0 +1,17 @@
+package com.rodrigo.cursojava.aula27.labs;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+
+		ContaCorrente contaRodrigo = new ContaCorrente();
+		contaRodrigo.numero = "123456";
+		contaRodrigo.agencia = "1234";
+		contaRodrigo.especial = true;
+		contaRodrigo.limiteEspecial = 300;
+		contaRodrigo.saldo = 180;
+		
+		contaRodrigo.IniciarMenu();
+		
+	}
+}
