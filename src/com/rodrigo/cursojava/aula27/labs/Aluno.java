1d16d9497a87a263aa728039ecbd1f2577dd31ed
rokalala <rodrigotavaresrt7@gmail.com>
2023-03-30 22:35:56 -0300

    quase lá

--- src/com/rodrigo/cursojava/aula27/labs/Aluno.java
+++ src/com/rodrigo/cursojava/aula27/labs/Aluno.java
@@ -1,9 +1,11 @@
 package com.rodrigo.cursojava.aula27.labs;
 
+import java.util.Scanner;
+
 public class Aluno {
 
 	String nome;
-	String mattricula;
+	String matricula;
 	String curso;
 	String[] disciplinas = new String[3];
 	int[] notasDisciplinas = new int[3];
@@ -23,6 +25,63 @@ public class Aluno {
 			}
 		}
 
+	}
+
+	void buscarAluno() {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.print("Nome: ");
+		nome = entrada.next();
+
+		System.out.print("Matricula: ");
+		matricula = entrada.next();
+
+		System.out.print("Curso: ");
+		curso = entrada.next();
+
+		for (String nomeDisciplina : disciplinas) {
+			System.out.print("Nome disciplina: ");
+			entrada.next();
+		}
+		
+		entrada.close();
+	}
+	
+	void mostrarNotas() {
+		
+		for (String disciplina : disciplinas) {
+			System.out.print(disciplina);
+			for (int nota : notasDisciplinas) {
+				System.out.println(" "+nota);
+			}
+		}
+		
+	}
+	
+	void exibirAluno() {
+		
+		System.out.println("Nome: " + nome);
+		System.out.println("Curso: " + curso);
+		System.out.println("Matrícula: " + matricula);
+		
+	}
+	
+	void verificarAprovado() {
+		
+		int somaNotas = 0;
+		
+		for (int nota : notasDisciplinas) {
+			somaNotas += nota;
+		}
+		
+		int mediaNota = somaNotas / 3;
+		
+		if (mediaNota >= 7) {
+			System.out.println(nome + ", Aprovado!");
+		} else {
+			System.out.println(nome + ", Reprovado!");
+		}
 	}
 	
 }
---
+++ src/com/rodrigo/cursojava/aula27/labs/Ex03.java
@@ -0,0 +1,14 @@
+package com.rodrigo.cursojava.aula27.labs;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		
+		Aluno aluno = new Aluno();
+		
+		aluno.buscarAluno();
+		aluno.verificarNota();
+		aluno.verificarAprovado();
+	}
+
+}
